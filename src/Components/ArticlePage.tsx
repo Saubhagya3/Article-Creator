8c0937a92059ca8489fa0f37b79befd436adc99d
Saubhagya <58411570+Saubhagya3@users.noreply.github.com>
2021-10-30 17:54:41 +1100

    Update ArticlePage.tsx

--- src/Components/ArticlePage.tsx
+++ src/Components/ArticlePage.tsx
@@ -6,20 +6,38 @@ import { Typography } from '@mui/material';
 import { GET_ALL_ARTICLES } from '../Graphql/Queries'
 import { useQuery } from '@apollo/client'
 import DrawerComponent from './DrawerComponent'
+import { Route, Link } from 'react-router-dom'
 
 export default function ArticlePage(props: any) {
     const { data } = useQuery(GET_ALL_ARTICLES)
     // const article = data.getAllArticles.find((art: number) => {
     //     return (parseInt(match.params.artId) === art.id)
     // })
+    const style = {
+        textDecoration: "none",
+        width: "15%",
+        minWidth: "4%",
+        border: "solid #78aee4",
+        borderRadius: "10px",
+        color: "white",
+        background: "#78aee4",
+        paddingTop: "5px",
+        paddingBottom: "5px",
+        marginTop: "30px",
+        float: "left" as "left"
+    }
     let getId = props.match.params.id
     const getData = data.getAllArticles[getId - 1]
     return(
         <Box sx={{ display: 'flex' }} style={{ padding: "0", margin: "0", marginTop: "50px"}}>
       
             <CssBaseline />
-            <DrawerComponent />
             <Box component="main" sx={{ p: 3, width: "100%" }}>
+                <Route>
+                    <Link to="/" style={style}>
+                        Back
+                    </Link>
+                </Route>
                 <Container maxWidth="sm">
                     <Box  style={{ padding: "15px",}} sx={{ bgcolor: '#ffffff', height: '100vh' }}>
                         <Typography variant="h4" component="div">
